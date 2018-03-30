a2ddfd3b7c1e0e53ea228fb53e557932921981a3
Guilherme <guilherme@minerva.kgi.edu>
2018-03-30 16:55:36 +0530

    add boards table

--- kanban/schema.sql
+++ kanban/schema.sql
@@ -1,6 +1,15 @@
 drop table if exists users;
+
 create table users (
   id integer primary key autoincrement,
   name text not null,
   email text not null unique
+);
+
+drop table if exists boards;
+create table boards (
+  id integer primary key autoincrement,
+  name text not null,
+  description text,
+  foreign key (author_id) references users(id)
 );
